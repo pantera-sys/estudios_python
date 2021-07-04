90111c7cdad764cc116c319b847f0f4c054cdab3
Jose Naranjo <josenaranjo2004.jn@gmail.com>
2021-07-04 15:08:26 -0500

    se añadio la funcion write para ver escribir en archivos en otro repositorio

--- archivos.py
+++ archivos.py
@@ -6,10 +6,14 @@ def read():
     print (numbers)
  
 def write():
-    pass
+    names = ["jose", "walter", "erika", "belen", "hola"]
+    with open ("./archivos/names.txt", "w", encoding="utf-8") as f:
+        for name in names: 
+            f.write(name)
+            f.write("\n")
 
 def run():
-    read()
+    write()
 
 if __name__ == "__main__":
     run()
